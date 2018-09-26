2784f58bc6e419ad7ef21b8c875badf670c6ca2d
linonetwo <linonetwo012@gmail.com>
2018-09-26 12:04:20 +0800

    bugfix: to save as single HTML should use memoryrouter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import { MDXProvider } from '@mdx-js/tag';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { MemoryRouter } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import { Container, Provider as RebassProvider } from 'rebass';
 import createComponents from '@rebass/markdown';
 import './GlobalStyles';
 import Routes from './Routes';
 
-const history = createBrowserHistory();
+const history = createMemoryHistory();
 class App extends Component {
   render() {
     return (
       <MDXProvider components={createComponents()}>
         <RebassProvider>
-          <Router history={history}>
+          <MemoryRouter history={history}>
             <Container>
               <Routes />
             </Container>
-          </Router>
+          </MemoryRouter>
         </RebassProvider>
       </MDXProvider>
     );
